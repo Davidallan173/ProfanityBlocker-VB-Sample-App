9d78974bc93a85f2a3d1f187970a0d04c21f2b54
David <management@cogsandgags.uk>
2019-10-03 14:03:21 +0100

    Add Sample app for the Profanity Blocker Visual Basic Implementation

---
+++ ConsoleApp1/ConsoleApp1/Module1.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Reflection
+
+Module Module1
+
+    Sub Main()
+        Dim oType As System.Type
+        Dim oAssembly As System.Reflection.Assembly
+        Dim oObject As System.Object
+
+        oAssembly = Assembly.LoadFrom("Path/To/Built/DLL")
+        oType = oAssembly.GetType("ProfanityBlocker.ProfanityBlocker")
+        oObject = Activator.CreateInstance(oType, "LICENCE", False, False, False)
+        While True
+            Dim input = Console.ReadLine()
+            Dim response As String = oObject.ParseText(input)
+            Console.WriteLine(response)
+        End While
+    End Sub
+
+End Module
